d8db28d8fe3e53e8386984f6c897a25927fc62ad
Sarah M Brown <brownsarahm@uri.edu>
2026-01-26 20:29:32 -0500

    fix module

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='syscourseutils',
-    version='1.0.8',
-    py_modules=['csssem',
+    name='semutils',
+    version='0.0.1',
+    py_modules=['csssem','csssem.cli','csssem.config'
                 ],
     install_requires=[
         'Click', 'pandas', 'lxml','pyyaml', 'numpy','requests','html5lib'
